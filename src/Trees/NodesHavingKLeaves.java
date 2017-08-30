2a7040a80ea293e1a0d50384695329e23e54e404
kalsi <sachinkalsi15@gmail.com>
2017-08-30 14:35:59 +0530

    Implement a function which returns list of all nodes in a binary tree having a given number of leaves, say k .

---
+++ src/Trees/NodesHavingKLeaves.java
@@ -0,0 +1,45 @@
+package Trees;
+
+/**
+ * Created by kalsi on 30/08/17.
+ * Implement a function which returns list of all nodes in a binary tree having a given number
+ of leaves, say k .
+ */
+public class NodesHavingKLeaves {
+    public static void main(String[] args) {
+        new NodesHavingKLeaves().demo();
+    }
+
+    private void demo() {
+        Node n = new Node(1);
+        //left
+        n.left = new Node(2);
+        n.left.left = new Node(5);
+        n.left.right = new Node(6);
+        n.left.left.left = new Node(9);
+        n.left.left.right = new Node(10);
+
+        //right
+        n.right = new Node(4);
+        n.right.left = new Node(7);
+        n.right.right = new Node(8);
+        n.right.left.left = new Node(11);
+        n.right.left.right = new Node(12);
+        displayNodes(n, 3);
+    }
+
+    private int displayNodes(Node n, int k) {
+        if (n == null) {
+            return 0;
+        }
+        if (n.left == null && n.right == null) {
+            return 1;
+        }
+        int temp = displayNodes(n.left, k) + displayNodes(n.right, k);
+        if (temp == k) {
+            System.out.println(n.data);
+        }
+        return temp;
+
+    }
+}
