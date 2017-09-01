9b36b872c8329a6e1c6aeb1daa57acd7946b42f5
kalsi <sachinkalsi15@gmail.com>
2017-09-02 00:11:45 +0530

    longest common subsequence back track

--- src/Strings/LongestCommon.java
+++ src/Strings/LongestCommon.java
@@ -17,8 +17,8 @@ public class LongestCommon {
     }
 
     public static void main(String[] args) {
-        String s1 = "abcdzaf";
-        String s2 = "zbcdp";
+        String s1 = "abcdaf";
+        String s2 = "zbcdf";
         new LongestCommon().findLongestCommon(s1, s2);
     }
 
@@ -33,55 +33,47 @@ public class LongestCommon {
         Index index = new Index();
         longest("substring", s1, s2, matrix, index);
         System.out.println("Length of Longest Common Substring: " + index.max);
-        printSubstring(matrix, index, s1);
+        printSubstring(matrix, index, s1, s2);
         index.max = 0;
         longest("subsequence", s1, s2, matrix, index);
         System.out.println("Length of Longest Common Subsequence: " + index.max);
-//        printSubsequence(matrix, s1, s2); // Not working
+        printSubsequence(matrix, s1, s2); // Not working
     }
 
     private void printSubsequence(int[][] matrix, String s1, String s2) {
         ArrayList<Character> al = new ArrayList<Character>();
-        printSubsequence(matrix, s1, s2, matrix.length - 1, matrix[0].length - 1, al);
+        StringBuilder sb = new StringBuilder();
+        printSubsequence(matrix, s1, s2, matrix.length - 1, matrix[0].length - 1, sb);
 
     }
 
-    private void printSubsequence(int[][] matrix, String s1, String s2, int m, int n, ArrayList<Character> al) {
+    private void printSubsequence(int[][] matrix, String s1, String s2, int m, int n, StringBuilder sb) {
         if (m == 0 || n == 0) {
-            for(Character c: al) {
-                System.out.print(c);
-            }
-//            System.out.println();
+            System.out.print("Subsequence is: " + sb.reverse().toString());
             return;
         }
         if (s1.charAt(m - 1) == s2.charAt(n - 1)) {
-            System.out.println(m);
-            al.add(s1.charAt(m - 1));
+            sb.append(s1.charAt(m - 1));
             m--;
             n--;
-            printSubsequence(matrix, s1, s2, m, n, al);
-        } else if (matrix[m - 1][n] == matrix[m][n - 1]) {
-            {
-                m--;
-                printSubsequence(matrix, s1, s2, m, n, new ArrayList<Character>(al));
-                n--;
-                m++;
-                printSubsequence(matrix, s1, s2, m, n, new ArrayList<Character>(al));
-            }
-        } else if (matrix[m - 1][n] > matrix[m][n - 1]) {
-            n--;
-            printSubsequence(matrix, s1, s2, m, n, al);
+            printSubsequence(matrix, s1, s2, m, n, sb);
+        } else if (matrix[m - 1][n] >= matrix[m][n - 1]) {
+            m--;
+            printSubsequence(matrix, s1, s2, m, n, sb);
 
         } else {
-            m--;
-            printSubsequence(matrix, s1, s2, m, n, al);
+            n--;
+            printSubsequence(matrix, s1, s2, m, n, sb);
         }
     }
 
-    private void printSubstring(int[][] matrix, Index index, String s) {
+    private void printSubstring(int[][] matrix, Index index, String s1, String s2) {
         StringBuilder sb = new StringBuilder();
         while (matrix[index.i][index.j] != 0) {
-            sb.append(s.charAt(index.i - 1));
+            if (s1.charAt(index.i - 1) == s2.charAt(index.j - 1)) {
+                sb.append(s1.charAt(index.i - 1));
+            }
+
             index.i--;
             index.j--;
         }
@@ -91,14 +83,19 @@ public class LongestCommon {
     private void longest(String type, String s1, String s2, int[][] matrix, Index index) {
         for (int i = 1; i <= s1.length(); i++) {
             for (int j = 1; j <= s2.length(); j++) {
-                matrix[i][j] = (type.equals("substring") ? matrix[i - 1][j - 1] : Math.max(matrix[i - 1][j], matrix[i][j - 1]));
+                if (type.equals("substring")) {
+                    matrix[i][j] = 0;
+                } else {
+                    matrix[i][j] = Math.max(matrix[i - 1][j], matrix[i][j - 1]);
+                }
+
                 if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
-                    matrix[i][j]++;
-                    if (matrix[i][j] > index.max) {
-                        index.max = matrix[i][j];
-                        index.i = i;
-                        index.j = j;
-                    }
+                    matrix[i][j] = matrix[i - 1][j - 1] + 1;
+                }
+                if (matrix[i][j] > index.max) {
+                    index.max = matrix[i][j];
+                    index.i = i;
+                    index.j = j;
                 }
 
             }
--- src/Strings/LongestPalindromSubsequence.java
+++ src/Strings/LongestPalindromSubsequence.java
@@ -3,6 +3,8 @@ package Strings;
 /**
  * Created by kalsi on 01/09/17.
  * http://www.geeksforgeeks.org/?p=19155
+ * https://github.com/mission-peace/interview/blob/master/src/com/interview/dynamic/LongestPalindromicSubsequence.java
+ * https://www.youtube.com/watch?v=_nCsPn7_OgI&t=1s
  */
 public class LongestPalindromSubsequence {
     public static void main(String[] args) {
@@ -11,7 +13,49 @@ public class LongestPalindromSubsequence {
 
     private void demo() {
         String s = "BBABCBCAB";
-        System.out.println(longest(s, 0, s.length() - 1));
+        System.out.println("Recursion: " + longest(s, 0, s.length() - 1));
+        System.out.println("Dynamic Programming: " + longestDP(s));
+    }
+
+    private int longestDP(String s) {
+        int[][] matrix = new int[s.length()][s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            matrix[i][i] = 1;
+        }
+        for (int length = 2; length <= s.length(); length++) {
+            for (int i = 0; i <= s.length() - length; i++) {
+                int j = i + length - 1;
+                if (length == 2 && s.charAt(i) == s.charAt(j)) {
+                    matrix[i][j] = 2;
+                } else if (s.charAt(i) == s.charAt(j)) {
+                    matrix[i][j] = 2 + matrix[i + 1][j - 1];
+                } else {
+                    matrix[i][j] = Math.max(matrix[i][j - 1], matrix[i + 1][j]);
+                }
+            }
+        }
+        displaySubsequence(s, matrix);
+        return matrix[0][s.length() - 1];
+    }
+
+    private void displaySubsequence(String s, int[][] matrix) {
+        int i = 0;
+        int j = matrix.length - 1;
+        StringBuilder sb = new StringBuilder();
+        while (i != j) {
+            if (s.charAt(i) == s.charAt(j)) {
+                sb.append(s.charAt(i));
+                i++;
+                j--;
+            } else {
+                if (matrix[i + 1][j] > matrix[i][j - 1]) {
+                    i++;
+                } else {
+                    j--;
+                }
+            }
+        }
+        System.out.println(sb.toString() + s.charAt(i) + sb.reverse().toString());
     }
 
     private int longest(String s, int start, int end) {
